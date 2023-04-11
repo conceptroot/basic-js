45d1896cb046cfb8d1bc61cd0d1371dc93832515
conceptroot <conceptroot@gmail.com>
2023-04-11 09:24:20 +0530

    feat: Carbon dating done ✅

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
+const E_LOG_2 = 0.693
 
 /**
  * Determine the age of archeological find by using
@@ -17,11 +18,30 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  console.log('#incoming sampleActivity:', sampleActivity)
+  if (typeof sampleActivity !== 'string') return false
+  if (!Number(sampleActivity)) return false
+
+
+  const initialActivity_A0 = MODERN_ACTIVITY
+  // console.log("A0:", initialActivity_A0)
+  const finalActivity_A = sampleActivity
+  // console.log("A:", finalActivity_A)
+  const N0_N = initialActivity_A0 / finalActivity_A  
+  // console.log("N0_N:", N0_N)
+  const rate_constant_k = E_LOG_2 / HALF_LIFE_PERIOD
+  // console.log("rate_onstant_k:", rate_constant_k)
+  const t = Math.log(N0_N) / rate_constant_k
+  // console.log("t", t)
+  
+  if (t < 0) return false
+  if (isNaN(t)) return false
+  return Math.ceil(t)
+
 }
 
 module.exports = {
   dateSample
 };
+
