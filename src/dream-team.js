2466c57413a5b7ba190a8e80433f7cae178846ba
conceptroot <conceptroot@gmail.com>
2023-04-11 09:50:57 +0530

    feat: Dream team done ✅

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(memberList) {
+  // console.log("#incoming memberList:", memberList)
+  if (!Array.isArray(memberList)) return false
+  let members = [...memberList]
+  members = members.filter(e => {
+    if (typeof e === "string" && e.length > 0) {return true}
+    return false
+  })
+  // console.log("#members:", members)
+  members = members.map(e => e.trim().toUpperCase())
+  // console.log("#members:", members)
+
+  let dreamNameArray = members.reduce((a,b) => {
+    // console.log("a:", a, "b:", b)
+    return [...a, b[0]]
+  }, [])
+  // console.log("#draemNameArray", dreamNameArray)
+  dreamNameArray = dreamNameArray.sort((a,b) => {
+    // console.log('$a:', a, '$b', b)
+    if (a < b) {
+      return -1 
+    }
+    return 1
+  })
+  // console.log("#draemNameArray", dreamNameArray)
+  return dreamNameArray.join('')
+
 }
 
 module.exports = {
   createDreamTeam
 };
+
