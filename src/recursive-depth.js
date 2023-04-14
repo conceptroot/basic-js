3a4fc1af4ba8072ba4a42a52132ab48f246be7fd
conceptroot <conceptroot@gmail.com>
2023-04-14 09:55:02 +0530

    feat: Recursive depth done ✅

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,12 +13,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.currentDepth = 0
+    this.maximumDepth = 0
+  }
+  calculateDepth(arr) {
+    console.log("#incoming arr:", arr)
+    this.currentDepth += 1
+    if (this.currentDepth > this.maximumDepth) this.maximumDepth = this.currentDepth
+    for (let element of arr) {
+      if (Array.isArray(element)) {
+        this.calculateDepth(element)
+      }
+    }
+    this.currentDepth -= 1
+    if (this.currentDepth === 0) {
+      const result = this.maximumDepth
+      this.maximumDepth = 0
+      return result
+    }
+    return this.maximumDepth
   }
 }
 
 module.exports = {
   DepthCalculator
 };
+
+// const depthCalc = new DepthCalculator();
+// console.log(depthCalc.calculateDepth([1, 2, 3, 4, 5]), '=> 1')
+// console.log(depthCalc.calculateDepth([1, 2, 3, [4, 5]]), '=> 2')
+// console.log(depthCalc.calculateDepth([[[]]]), '=> 3')
+
+// console.log(depthCalc.calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), '=> 31')
+// console.log(depthCalc.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), '=> 5')
+// console.log(depthCalc.calculateDepth([1, 2, 3, 4, 5, [1]]), '=> 2')
+
