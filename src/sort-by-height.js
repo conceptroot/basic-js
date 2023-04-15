4a7e2ec5bf3fdcd318d818f6af08b23560ca96b6
conceptroot <conceptroot@gmail.com>
2023-04-16 01:11:40 +0530

    feat: (ST) Sort by height done ✅

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,11 +11,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  
+
+function sortByHeight(arr) {
+  // console.log("#arr:", arr)
+  // забрать индексы и значениya -1
+  const _ = splitWrongValues(arr)
+  const properArray = _.properArray
+  const wrongValuesMap = _.wrongValuesMap
+  // console.log("#properArray:", properArray)
+  // console.log("#wrongValuesMap:", wrongValuesMap)
+  // отсортировать оставшийся массив
+  const sortedArray = properArray.sort((a,b) => a-b ? a-b:a+b)
+  // console.log("#sortedArray", sortedArray)
+  // добавить в массив индексы и значения из 1 шага
+  const result = combineSortedAndWrong(sortedArray, wrongValuesMap)
+  // console.log("#result", result)
+  return result
+  
+}
+function combineSortedAndWrong(sortedArray, wrongMap) {
+  const result = [...sortedArray]
+  wrongMap.forEach((v, i) => {
+    result.splice(i, 0, v)
+  })
+  return result
+}
+
+function splitWrongValues(arr) {
+  const wrongValuesMap = new Map()
+  arr.forEach((v, i) => {
+    if (v === -1) {
+      wrongValuesMap.set(i, v)
+    }
+  })
+  const properArray = arr.filter(v => v !== -1)
+  return {
+    properArray: properArray,
+    wrongValuesMap: wrongValuesMap
+  }
 }
 
 module.exports = {
   sortByHeight
 };
+
+// console.log("=".repeat(40))
+// console.log(
+      // sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]),
+      // [-1, 150, 160, 170, -1, -1, 180, 190],
+    // );
