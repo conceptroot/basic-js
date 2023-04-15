8f7eeec2e9c2a42c7b0e0c6030ebd5c78228a764
conceptroot <conceptroot@gmail.com>
2023-04-15 19:22:13 +0530

    feat: (ST) Encode line done ✅

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  // console.log('#incoming str:', str)
+  let buffer = [] 
+  let result = ''
+  for (let i=0; i<str.length; i++) {
+    const currentLetter = str[i]
+    // console.log('buffer.slice', buffer.slice(-1))
+    if (buffer.slice(-1)[0] === currentLetter) {
+      buffer.push(currentLetter)
+    } else {
+      if (buffer.length) {
+        const count = buffer.length === 1 ? '' : buffer.length
+        const letter = buffer.slice(-1)
+        result += `${count}${letter}`
+      }
+      buffer = [currentLetter]
+    }
+  }
+  if (buffer.length) {
+    const count = buffer.length === 1 ? '' : buffer.length
+    const letter = buffer.slice(-1)
+    result += `${count}${letter}`
+  }
+
+  return result
 }
 
 module.exports = {
   encodeLine
 };
+
+// console.log(encodeLine('aaaatttt'), '4a4t');
+// console.log(encodeLine('aabbccc'), '2a2b3c');
+// console.log(encodeLine('abbcca'), 'a2b2ca');
+// console.log(encodeLine('xyz'), 'xyz');
+// console.log(encodeLine(''), '');
