ce7ab694a0a1649394543f3ffdb78544c25ca33c
conceptroot <conceptroot@gmail.com>
2023-04-15 20:11:27 +0530

    feat: (ST) Get email domain done ✅

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,11 +10,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  // console.log('#email:', email)
+  const atIndex = email.lastIndexOf('@')
+
+  const result = email.split('').slice(atIndex + 1).join('')
+  return result
+
 }
 
 module.exports = {
   getEmailDomain
 };
+
+// console.log(getEmailDomain('prettyandsimple@example.com'), 'example.com');
+// console.log(getEmailDomain('someaddress@yandex.ru'), 'yandex.ru');
+// console.log(getEmailDomain('very.unusual.@.unusual.com@usual.com'), 'usual.com');
+// console.log(getEmailDomain('admin@mailserver2.ru'), 'mailserver2.ru');
+// console.log(getEmailDomain('example-indeed@strange-example.com'), 'strange-example.com');
